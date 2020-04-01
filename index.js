76082e8f676172f975802adcac1e8fecb7cf7efe
Carson Keller <carsonjkeller@gmail.com>
2020-04-01 09:51:07 -0600

    Second commit - added external package and image

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("Hello World!")
+const replaceColor = require('replace-color')
+
+console.log(helloWorld);
+
+console.log('image.png')
+
+replaceColor({
+  image: 'image.png',
+  colors: {
+    type: 'hex',
+    targetColor: '#00D4B9',
+    replaceColor: '#CB4947'
+  }
+}, (err, jimpObject) => {
+  if (err) return console.log(err)
+  jimpObject.write('./output.jpg', (err) => {
+    if (err) return console.log(err)
+  })
+})
+
+console.log('image.jpg')
